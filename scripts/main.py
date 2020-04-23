ca8ad4aa637d6c9f369e492704233c4fa5b1bf3c
HONGRUNYU <michaelhong1995@gmail.com>
2020-04-23 17:47:16 -0400

    4.23.1

--- scripts/main.py
+++ scripts/main.py
@@ -93,33 +93,84 @@ def CAM(net, w, pred, x, path, name, bs, rd=0):
             cv2.imwrite(imname3, full)
 
 
+def inference(images, num_classes, for_training=False, restore_logits=True,
+              scope=None):
+  """Build Inception v3 model architecture.
+
+  See here for reference: http://arxiv.org/abs/1512.00567
+
+  Args:
+    images: Images returned from inputs() or distorted_inputs().
+    num_classes: number of classes
+    for_training: If set to `True`, build the inference model for training.
+      Kernels that operate differently for inference during training
+      e.g. dropout, are appropriately configured.
+    restore_logits: whether or not the logits layers should be restored.
+      Useful for fine-tuning a model with different num_classes.
+    scope: optional prefix string identifying the ImageNet tower.
+
+  Returns:
+    Logits. 2-D float Tensor.
+    Auxiliary Logits. 2-D float Tensor of side-head. Used for training only.
+  """
+
+  # Set weight_decay for weights in Conv and FC layers.
+  with slim.arg_scope([slim.ops.conv2d, slim.ops.fc], weight_decay=0.00004):
+    with slim.arg_scope([slim.ops.conv2d],
+                        stddev=0.1,
+                        activation=tf.nn.relu,
+                        batch_norm_params={'decay': 0.9997, 'epsilon': 0.001}):
+      logits, endpoints, net2048, sel_endpoints = slim.inception.inception_v3(
+          images,
+          dropout_keep_prob=0.8,
+          num_classes=num_classes,
+          is_training=for_training,
+          restore_logits=restore_logits,
+          scope=scope)
+
+  # Grab the logits associated with the side head. Employed during training.
+  auxiliary_logits = endpoints['aux_logits']
+
+  #return logits, auxiliary_logits
+  return logits, auxiliary_logits, endpoints, net2048, sel_endpoints
+
+
 if __name__ == "__main__":
     filename = '../Results/test.tfrecords'
     datasets = data_input.DataSet(100, filename=filename)
     itr, file, ph = datasets.data()
     next_element = itr.get_next()
+    with tf.Session() as sesss:
+        sesss.run(itr.initializer, feed_dict={ph: file})
+        x = sesss.run(next_element)
 
-    with tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=True)) as sess:
-        sess.run(itr.initializer, feed_dict={ph: file})
-        saver = tf.train.import_meta_graph('../model/model.ckpt-31500.meta')
-        saver.restore(sess, '../model/model.ckpt-31500')
-        # print(sess.run('logits/logits/weights:0'))
-        print_tensors_in_checkpoint_file(file_name='../model/model.ckpt-31500', tensor_name='',
-                                         all_tensors=False, all_tensor_names=False)
-        # graph = tf.get_default_graph()
-        # x = sess.run(next_element)
-        # feed_dict = {images: x, num_classes: 2}
 
-        # with slim.arg_scope([slim.ops.conv2d, slim.ops.fc], weight_decay=0.00004):
-        #     with slim.arg_scope([slim.ops.conv2d],
-        #                         stddev=0.1,
-        #                         activation=tf.nn.relu,
-        #                         batch_norm_params={'decay': 0.9997, 'epsilon': 0.001}):
-        #         logits, endpoints, net2048, sel_endpoints = slim.inception.inception_v3(
-        #               x,
-        #               dropout_keep_prob=0.8,
-        #               num_classes=2,
-        #               is_training=False,
-        #               restore_logits=True,
-        #               scope=None)
+    # saver = tf.train.import_meta_graph('../model/model.ckpt-31500.meta')
+
+    # print(sess.run('logits/logits/weights:0'))
+    # print_tensors_in_checkpoint_file(file_name='../model/model.ckpt-31500', tensor_name='',
+    #                                  all_tensors=False, all_tensor_names=False)
+    with tf.Graph().as_default():
+        # image input
+        x_in = tf.placeholder(tf.float32, name="x")
+        x_in_reshape = tf.reshape(x_in, [-1, 299, 299, 3])
+        logits, _, end_points, net2048, sel_end_points = inference(x_in_reshape, 2)
+        # # Restore the moving average version of the learned variables for eval.
+        # variable_averages = tf.train.ExponentialMovingAverage(
+        #     0.9997)
+        # variables_to_restore = variable_averages.variables_to_restore()
+        # saver = tf.train.Saver(variables_to_restore)
+
+        with tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=True)) as sess:
+            tf.global_variables_initializer().run()
+            saver = tf.train.import_meta_graph('../model/model.ckpt-31500.meta')
+            saver.restore(sess, '../model/model.ckpt-31500')
+            # x_in = tf.convert_to_tensor(x)
+            net2048_, end_points_, logits_, x_= sess.run([logits, end_points, net2048, x_in_reshape], {x_in: x})
+
+    print(logits_)
+    print(end_points_)
+    print(net2048_)
+
+
 
